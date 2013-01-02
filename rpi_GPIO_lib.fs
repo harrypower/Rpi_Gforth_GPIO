aba86f672e68040c396b9ab38c01fe11b3b7449c
root <root@raspberrypi.(none)>
2013-01-02 14:37:18 -0700

    changed rpi_GPIO_lib.fs comments to show proper basic usage

--- rpi_GPIO_lib.fs
+++ rpi_GPIO_lib.fs
@@ -1,15 +1,12 @@
 \ This gforth code is used as follows:
 \ include forth_lib_example.fs	\ this will load this file into gforth and compile and run it
-\ *** NOTE the file named librpigpio.a needs to be placed into /usr/lib directory 
-\ *** This library is refered to in the below add-lib command.  I have not yet figured out how to 
-\ *** give the add-lib a path to the library but the standard library path is used and works fine.
-\ The library does not need to be a shared library.  I have only tested with dynamic librarys but it should work with static ones also
+
 
 c-library mypigpio
 s" rpigpio" add-lib
 \c #include <sys/types.h>
 \c #include <unistd.h>
-\c #include "/home/pi/git/Rpi_Gforth_GPIO/c_src/rpiGpio.h"
+\c #include "c_src/rpiGpio.h"
 \c int pipinoutput(int);
 \c int pipinlow(int);
 \c int pipinhigh(int);
