19dfd06a0bff5ec6007f6269feb367776df320dc
Philip Smith <philipkingsmith@gmail.com>
2013-01-03 00:20:53 -0700

    Added gpl to forth files.  Added function to forth library.
    Added pipininput function to rpi_GPIO_lib.fs

--- rpi_GPIO_example.fs
+++ rpi_GPIO_example.fs
@@ -1,6 +1,26 @@
+\ This Gforth code is a Raspberry Pi GPIO library
+\    Copyright (C) 2013  Philip K. Smith
+
+\    This program is free software: you can redistribute it and/or modify
+\    it under the terms of the GNU General Public License as published by
+\    the Free Software Foundation, either version 3 of the License, or
+\    (at your option) any later version.
+
+\    This program is distributed in the hope that it will be useful,
+\    but WITHOUT ANY WARRANTY; without even the implied warranty of
+\    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\    GNU General Public License for more details.
+
+\    You should have received a copy of the GNU General Public License
+\    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\ This gforth code is used as follows:
+\ include forth_lib_example.fs	\ this will load this file into gforth and compile and run it.
+\ Note remember to ajust the path to the #include in below code if needed!
+
 require rpi_GPIO_lib.fs
 
-: simpleOutput ( -- )
+: simpleoutput ( -- )
 piosetup 0= if 
 25 pisetoutput 0= if s" pin 25 set for output" type cr endif
 25 pipinlow 0= if s" pin 25 is low now" type cr endif
--- rpi_GPIO_lib.fs
+++ rpi_GPIO_lib.fs
@@ -1,3 +1,19 @@
+\ This Gforth code is a Raspberry Pi GPIO library
+\    Copyright (C) 2013  Philip K. Smith
+
+\    This program is free software: you can redistribute it and/or modify
+\    it under the terms of the GNU General Public License as published by
+\    the Free Software Foundation, either version 3 of the License, or
+\    (at your option) any later version.
+
+\    This program is distributed in the hope that it will be useful,
+\    but WITHOUT ANY WARRANTY; without even the implied warranty of
+\    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\    GNU General Public License for more details.
+
+\    You should have received a copy of the GNU General Public License
+\    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 \ This gforth code is used as follows:
 \ include forth_lib_example.fs	\ this will load this file into gforth and compile and run it.
 \ Note remember to ajust the path to the #include in below code if needed!
@@ -10,9 +26,11 @@ s" rpigpio" add-lib
 \c int pipinoutput(int);
 \c int pipinlow(int);
 \c int pipinhigh(int);
+\c int pipininput(int pin) { return ( gpioSetFunction ( pin, input )) ; }
 \c int pipinoutput(int pin) { return ( gpioSetFunction ( pin, output )) ; }
 \c int pipinlow(int pin) { return ( gpioSetPin ( pin, low )) ; }
 \c int pipinhigh(int pin) { return ( gpioSetPin ( pin, high )) ; }
+c-function pipininput pipininput n -- n
 c-function pisetoutput pipinoutput n -- n 
 c-function piosetup gpioSetup -- n
 c-function piocleanup gpioCleanup -- n
@@ -20,11 +38,12 @@ c-function pipinlow pipinlow n -- n
 c-function pipinhigh pipinhigh n -- n
 end-c-library
 
-piosetup .	\ this should do basic setup of the gpio function and show errStatus value (0 for all ok)
-25 pisetoutput .
-25 pipinlow .
-1000 ms
-25 pipinhigh .
-piocleanup .	\ this should stop the gpio function and show errStatus value ( 0 for all ok)
+\ basic output on gpio pin 25 example
+\ piosetup .	\ this should do basic setup of the gpio function and show errStatus value (0 for all ok)
+\ 25 pisetoutput .
+\ 25 pipinlow .
+\ 1000 ms
+\ 25 pipinhigh .
+\ piocleanup .	\ this should stop the gpio function and show errStatus value ( 0 for all ok)
 
 
