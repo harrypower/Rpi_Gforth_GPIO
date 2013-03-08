9836638a8999a30df56c52ebf7df69b6c79efb7c
harrypower <philipkingsmith@gmail.com>
2013-03-08 15:39:08 -0700

    Added read date and time and set date and time functions to ds1307 library Fixed error detection stuff in ds1307 library ( throw used incorreclty)

--- ds1307_i2c_lib.fs
+++ ds1307_i2c_lib.fs
@@ -24,55 +24,75 @@
 include rpi_GPIO_lib.fs
 
 : valid_addr ( reg_addr -- reg_addr flag )	\ 0 to 63 are only valid addresses.  Flag returns zero for correct addresses.
-	dup 0< if 11 else dup 63 > if 11 else 0 then then ;	
+    dup 0< if 11 else dup 63 > if 11 else 0 then then ;	
 
-: !ds1307 ( val reg_addr -- flag )	\ Write val to ds1307 reg_addr.  Flag is zero for success. 
-	TRY 	valid_addr throw
-		piosetup throw pii2csetup throw 104 pii2caddress throw 100000 pii2clock throw
-		pad c! pad 1 + c! pad 2 pii2cwrite throw pii2cleanup throw piocleanup throw 0
- 	ENDTRY-IFERROR	swap drop swap drop	\ ds1307 writing failed error # is on stack at exit of this code
-	THEN					\ all ok no errors
-	;
+: !ds1307 ( val reg_addr -- flag )		\ Write val to ds1307 reg_addr.  Flag is zero for success. 
+    TRY
+	valid_addr throw
+	piosetup throw pii2csetup throw 104 pii2caddress throw 100000 pii2clock throw
+	pad c! pad 1 + c! pad 2 pii2cwrite throw pii2cleanup throw piocleanup throw 0
+    RESTORE dup if swap drop swap drop  then	\ ds1307 writing failed error # is on stack at exit of this code
+    ENDTRY ;
 
-: @ds1307 ( -- val flag )	\ Read next ds1307 register value.  Flag is zero for success.
-	TRY	piosetup throw pii2csetup throw 104 pii2caddress throw 100000 pii2clock throw
-		pad 1 pii2cread throw pii2cleanup throw piocleanup throw pad c@ 0 
-	ENDTRY-IFERROR	0 swap	\ read error # is on stack at exit of this code
-	THEN			\ read ok no errors 
-	;
+: @ds1307 ( -- val flag )		\ Read next ds1307 register value.  Flag is zero for success.
+    TRY
+	piosetup throw pii2csetup throw 104 pii2caddress throw 100000 pii2clock throw
+	pad 1 pii2cread throw pii2cleanup throw piocleanup throw pad c@ 0 
+    RESTORE	dup if 11 swap  then	\ read error # is on stack at exit of this code
+    ENDTRY ;				\ read ok no errors 
+	
 
 : @ds1307_reg	( reg_addr -- val flag )	\ Read reg_addr on ds1307.  Flag is zero for success.
 	TRY	valid_addr throw
 		0 63 !ds1307 throw		\ This clobers reg 63 or the last ram location in ds1307
 		0 ?DO @ds1307 throw drop LOOP 	\ Now reg_addr is the next address to be read.
 		@ds1307 			
-	ENDTRY-IFERROR
-	THEN
+	RESTORE 
+	ENDTRY
 	;
-
+	
 : ds1307_clock_off ( -- flag ) \ Turn on clock
     TRY
 	0 @ds1307_reg throw 128 or 0 !ds1307 
-    ENDTRY-IFERROR
-    THEN ;
+    RESTORE 
+    ENDTRY ;
 
 : ds1307_clock_on ( -- flag )  \ Clock off
     TRY
 	0 @ds1307_reg throw 127 and 0 !ds1307
-    ENDTRY-IFERROR
-    THEN ;
+    RESTORE
+    ENDTRY ;
 
-: dectobcd ( n -- n )
+: dectobcd ( n -- n ) \ 8 bit dec to binary coded decimal 
     dup 10 / dup 16 * swap 10 * rot swap - + ;
 
-: bcdtodec ( n -- n )
+: bcdtodec ( n -- n ) \ 8 bit binary coded decimal to dec
     dup 16 / dup 10 * swap 16 * rot swap - + ;
 
 : @ds1307_time ( -- sec min hour flag )  \ Read seconds, minutes, hours (24 hour format).  Flag zero for correct reading
     TRY
 	0 @ds1307_reg throw 127 and bcdtodec @ds1307 throw 127 and bcdtodec @ds1307 throw  \ reading done
 	dup 64 and if dup 32 and if 12 else 0 then swap 31 and bcdtodec + else 63 and bcdtodec then 0
-    ENDTRY-IFERROR 0 swap 0 swap 0 swap    \ some read error
-    THEN ;
+    RESTORE dup if 11 swap 11 swap 11 swap then    \ some read error
+    ENDTRY ;
 
-\ test
+: !ds1307_time ( sec min hour -- flag ) \ Set the time in 24 hour format.  Flag zero for no errors
+    TRY
+	31 and dectobcd 2 !ds1307 throw 63 and dectobcd 1 !ds1307 throw 63 and 0 !ds1307 throw ds1307_clock_on throw 0 
+    RESTORE dup if swap drop swap drop swap drop then
+    ENDTRY ;
+
+: @ds1307_date ( -- day date month year flag ) \ day is 1-7, date is 1-31, month 1-12, year 2000-2099.  Flag zero if read was successfull
+    TRY
+	3 @ds1307_reg throw 7 and bcdtodec @ds1307 throw 63 and bcdtodec @ds1307 throw 31 and bcdtodec @ds1307 throw bcdtodec 2000 + 0
+    RESTORE dup if 11 swap 11 swap 11 swap 11 swap then  
+    ENDTRY ;
+
+: !ds1307_date ( day date month year -- flag ) \ day is 1-7, date is 1-31, month 1-12, year 2000-2099. Flag zero indicates successfull write
+    TRY
+	dup 2000 >= if 2000 - dup 99 <= if dectobcd 6 !ds1307 throw else 11 throw then else 11 throw then	\ set year 
+	dup 1 >= if dup 12 <= if dectobcd 5 !ds1307 throw else 11 throw then  else 11 throw then		\ set month
+	31 and dectobcd 4 !ds1307 throw		\ set date
+	7 and dectobcd 3 !ds1307 throw 0	\ set day
+    RESTORE dup if swap drop swap drop swap drop swap drop then
+    ENDTRY ;
